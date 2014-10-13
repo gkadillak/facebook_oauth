2a7a9e38ec231fae80ce961906da9d839ab8357d
Garrett Kadillak <g.kadillak@comcast.net>
2014-10-13 12:26:24 -0700

    Cleaned up views.py and settings for code review.

--- feedr/urls.py
+++ feedr/urls.py
@@ -10,8 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^$', 'friend_finder.views.home', name='home'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^profile/$', 'friend_finder.views.profile', name='profile'),
-    url(r'^maps/$', 'friend_finder.views.maps', name='maps'),
 
 )
--- friend_finder/views.py
+++ friend_finder/views.py
@@ -12,28 +12,6 @@ def home(request):
     return render(request, 'index.html')
 
 
-def instagram_login(request):
-    request.user.social_auth_filter(provider='instagram').first()
-    instagram_login()
-    instagram.InstagramAPI()
-
-
-@login_required
-def profile(request):
-    user_social_auth = request.user.social_auth.filter(provider='facebook').first()
-    graph = facebook.GraphAPI(user_social_auth.extra_data['access_token'])
-    profile_data = graph.get_object("me/picture", height="200")
-
-    return render(request, 'profile.html', profile_data)
-
-
-
-def maps(request):
-    user_social_auth = request.user.social_auth.filter(provider='facebook').first()
-    graph = facebook.GraphAPI(user_social_auth.extra_data['access_token'])
-    photos = graph.get_object("me/photos", limit=50)
-
-    return render(request, 'map.html', {'photo_json': json.dumps(photos['data'])})
 
 
 
